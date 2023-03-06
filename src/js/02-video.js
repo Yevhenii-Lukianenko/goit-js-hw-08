7c0f6cea0d8585f1ca307f0d2221da8dcdaa3396
Yevhenii-Lukianenko <zhenyalukyanenko17@gmail.com>
2023-03-06 16:53:40 +0200

    shortened code

--- src/js/02-video.js
+++ src/js/02-video.js
@@ -5,11 +5,18 @@ const iframeRef = document.querySelector('iframe');
 const player = new Player(iframeRef);
 
 const STORAGE_KEY = 'videoplayer-current-time';
+const isSTORAGE_KEY = localStorage.getItem(STORAGE_KEY);
 
 player.on('timeupdate', throttle(onPlay, 1000));
+
+if (isSTORAGE_KEY) {
+  onLoadPlayer();
+}
 
 function onPlay({ seconds }) {
   localStorage.setItem(STORAGE_KEY, seconds);
 }
 
-player.setCurrentTime(localStorage.getItem(STORAGE_KEY));
+function onLoadPlayer() {
+  player.setCurrentTime(isSTORAGE_KEY);
+}
